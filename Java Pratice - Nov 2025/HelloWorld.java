565f08def0cb5054344726004d62447929fcbe2d
arsal3646 <arsal3646@gmail.com>
2025-11-10 04:10:30 +0400

    hi

--- Java Pratice - Nov 2025/HelloWorld.java
+++ Java Pratice - Nov 2025/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello !!!!!!! I am trying to learn Java. First step is to connect to GitHub + VS Code Web");
+        System.out.println("Hello World! I am new to Java. I just completed the first step, i.e. to connect to GitHub + VS Code Web");
     }
 }
